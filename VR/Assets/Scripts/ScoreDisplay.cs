34f7da22afb2702ad37ef62fe1beecfee6c6582f
SlenderFox <SlenderFox@outlook.com>
2019-06-19 09:56:58 +1000

    Upgraded the text boxes

--- VR/Assets/Scripts/ScoreDisplay.cs
+++ VR/Assets/Scripts/ScoreDisplay.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    public Text m_score;
-    public Text m_combo;
+    public TMPro.TextMeshProUGUI m_score;
+    public TMPro.TextMeshProUGUI m_combo;
     public Frisbee m_frisbee;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,5 @@ public class ScoreDisplay : MonoBehaviour
     {
         m_score.text = "Goals: " + m_frisbee.m_score.ToString();
         m_combo.text = "Combo: " + m_frisbee.m_combo.ToString();
-
     }
 }
